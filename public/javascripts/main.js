2da3fe6140ddce007e247e32e10cd5773bd342c9
Ashley <ahappyday4all@gmail.com>
2021-04-30 23:58:32 +0800

    feat: add valuable feedback

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,3 +2,18 @@
 function deleteAlert() {
   return confirm('Are you sure you want to delete this restaurant ?')
 }
+
+/* Submit form */
+function submitForm(event) {
+  const form = document.querySelector('#form')
+  if (!form.checkValidity()) {
+    event.preventDefault()
+    event.stopPropagation()
+  }
+}
+
+/* Click submit button */
+function clickSubmit() {
+  const form = document.querySelector('#form')
+  form.classList.add('was-validated')
+}
